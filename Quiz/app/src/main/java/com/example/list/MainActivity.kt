a97fa79d285babfd3d3539b64f496b9dcc2c2f43
Roman Pelikh <pelikh.roman@gmail.com>
2020-02-07 11:22:24 -0400

    add about and overflow menu

--- Quiz/app/src/main/java/com/example/list/MainActivity.kt
+++ Quiz/app/src/main/java/com/example/list/MainActivity.kt
@@ -5,81 +5,35 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.list.databinding.ActivityMainBinding
 import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
 
-    //Create list of questions
-    private val questionBank = listOf(
-        Question(R.string.question_1, false),
-        Question(R.string.question_2, true),
-        Question(R.string.question_3, true),
-        Question(R.string.question_4, false),
-        Question(R.string.question_5, false),
-        Question(R.string.question_6, true),
-        Question(R.string.question_7, false),
-        Question(R.string.question_8, true),
-        Question(R.string.question_9, false),
-        Question(R.string.question_10, false),
-        Question(R.string.question_11, false),
-        Question(R.string.question_12, true),
-        Question(R.string.question_13, false),
-        Question(R.string.question_14, true),
-        Question(R.string.question_15, false),
-        Question(R.string.question_16, false),
-        Question(R.string.question_17, true),
-        Question(R.string.question_18, false),
-        Question(R.string.question_19, false),
-        Question(R.string.question_20, true)
-    )
-
-    private var questionIndex = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        //Add click listener to button Next
-        findViewById<Button>(R.id.next_button).setOnClickListener { nextQ() };
-        findViewById<Button>(R.id.perv_button).setOnClickListener { prevQ() }
-        findViewById<Button>(R.id.trueButton).setOnClickListener { answer(true) }
-        findViewById<Button>(R.id.falseButton).setOnClickListener { answer(false) }
-    }
-
-
-    private fun nextQ() {
-        //Get question index. If it is the last question it will be reseted to 0
-        questionIndex = (questionIndex + 1) % 20;
-        updateView();
+        //ADD BACK BUTTON
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        //Link the navigation controller to the app navbar
+        NavigationUI.setupActionBarWithNavController(this,navController)
+        
+        
 
     }
 
-    private fun prevQ() {
-        questionIndex = if (questionIndex == 0) {
-            questionBank.size - 1;
-        } else {
-            (questionIndex - 1) % 20;
-        }
-
-      //questionIndex =  20 - abs(questionIndex - 1) % 20;
-
-        updateView();
+    //NAVIGATE UP WHEN ARROW IN NAVBAR IS PRESSED
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 
 
-    private fun answer(ans: Boolean) {
-        if (ans == questionBank[questionIndex].answer) {
-            Toast.makeText(applicationContext, "CORRECT", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(applicationContext, "WRONG", Toast.LENGTH_SHORT).show();
-        }
-    }
 
-    private fun updateView() {
-        //Find TextView for question and set question from QuestionBank
-        //This resourceId will get text from this resource id
-        findViewById<TextView>(R.id.txt_question).setText(questionBank[questionIndex].resourceId);
-    }
 
 }
---
+++ Quiz/app/src/main/java/com/example/list/MainFragment.kt
@@ -0,0 +1,137 @@
+package com.example.list
+
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.list.databinding.FragmentMainBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding;
+    private var questionIndex = 0;
+
+    //Create list of questions
+    private val questionBank = listOf(
+        Question(R.string.question_1, false),
+        Question(R.string.question_2, true),
+        Question(R.string.question_3, true),
+        Question(R.string.question_4, false),
+        Question(R.string.question_5, false),
+        Question(R.string.question_6, true),
+        Question(R.string.question_7, false),
+        Question(R.string.question_8, true),
+        Question(R.string.question_9, false),
+        Question(R.string.question_10, false),
+        Question(R.string.question_11, false),
+        Question(R.string.question_12, true),
+        Question(R.string.question_13, false),
+        Question(R.string.question_14, true),
+        Question(R.string.question_15, false),
+        Question(R.string.question_16, false),
+        Question(R.string.question_17, true),
+        Question(R.string.question_18, false),
+        Question(R.string.question_19, false),
+        Question(R.string.question_20, true)
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate<FragmentMainBinding>(
+            inflater,
+            R.layout.fragment_main, container, false
+        )
+
+
+        //ADD CLICK LISTENERS TO BUTTONS
+        binding.nextButton.setOnClickListener {
+            nextQ()
+        }
+
+        binding.pervButton.setOnClickListener {
+            prevQ();
+        }
+
+        binding.trueButton.setOnClickListener {
+            answer(true)
+        }
+        binding.falseButton.setOnClickListener {
+            answer(false)
+        }
+
+
+        //TELL VIEW THAT IT HAS AN OPTION MENU, SO IT WILL ENABLE IT
+        setHasOptionsMenu(true)
+
+
+        return binding.root
+    }
+
+
+    private fun nextQ() {
+        //Get question index. If it is the last question it will be reseted to 0
+        questionIndex = (questionIndex + 1) % 20;
+        updateView();
+
+    }
+
+    private fun prevQ() {
+        questionIndex = if (questionIndex == 0) {
+            questionBank.size - 1;
+        } else {
+            (questionIndex - 1) % 20;
+        }
+
+        //questionIndex =  20 - abs(questionIndex - 1) % 20;
+
+        updateView();
+    }
+
+
+    private fun answer(ans: Boolean) {
+        if (ans == questionBank[questionIndex].answer) {
+            Toast.makeText(requireContext(), "CORRECT", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(requireContext(), "WRONG", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private fun updateView() {
+        //Find TextView for question and set question from QuestionBank
+        //This resourceId will get text from this resource id
+        binding.txtQuestion.setText(questionBank[questionIndex].resourceId)
+
+    }
+
+
+    //INFLATE OPTIONS MENU ITEM ON UI
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.options_menu, menu)
+    }
+
+
+    //ADD CLICK HANDLER TO OPTIONS MENU
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //CLICK HANDLER MUST NAVIGATE US TO ABOUT FRAGMENT
+
+        //IF WE HAVE CLICK HANDLER THAT SHOULD NAVIGATE US TO FRAGMENT -> GO TO THAT FRAGMENT ELSE DO WHATEVER ACTION ON ITEM SELECTED
+        return NavigationUI.onNavDestinationSelected(
+            item!!,
+            view!!.findNavController()
+        ) || super.onOptionsItemSelected(item)
+    }
+
+}
